c61a1cd203daa3e570f87f6793f585bc61f33f96
Guan-Xin <alex0972112871@gmail.com>
2023-04-18 15:09:23 +0800

    fix: refactor the chapter standalone component for reducing duplicate template and improve readibility.

--- apps/code-editor/src/app/components/chapter/chapter.component.ts
+++ apps/code-editor/src/app/components/chapter/chapter.component.ts
@@ -1,8 +1,10 @@
 import { MatIconModule } from '@angular/material/icon';
 import {
+  ChangeDetectorRef,
   Component,
   EventEmitter,
   Input,
+  OnDestroy,
   Output,
   ViewEncapsulation,
 } from '@angular/core';
@@ -16,7 +18,7 @@ import { Chapter } from '@game/data-access/code-editor-data';
 import { ToggleAnswerDirective } from '../../directives/toggle-answer.directive';
 import { EditorComponent } from '../editor/editor.component';
 import { ChapterEntityService } from '../../services/chapter-entity.service';
-import { first, tap, withLatestFrom } from 'rxjs';
+import { Subject, take, takeUntil, tap, withLatestFrom } from 'rxjs';
 import { EditorService } from '../../services/editor.service';
 import { FormControl } from '@angular/forms';
 
@@ -35,42 +37,78 @@ import { FormControl } from '@angular/forms';
     MatInputModule,
   ],
   template: `
-    <div class="chapter">
+    <div class="chapter" [ngSwitch]="mode">
       <markdown
-        *ngIf="!editMode && !createMode"
+        *ngSwitchCase="'default'"
         class="chapter__content"
         [data]="chapter?.content?.join('\\n')"></markdown>
-      <!-- edit mode with editor -->
-      <div *ngIf="editMode">
-        <div
-          class="chapter__form"
-          style="display: flex; justify-content: space-between;">
-          <mat-form-field appearance="fill">
-            <mat-label>Enter chapter title</mat-label>
-            <input
-              matInput
-              placeholder="introduction"
-              [formControl]="title"
-              required />
-          </mat-form-field>
-          <mat-form-field appearance="fill">
-            <mat-label>Enter chapter order</mat-label>
-            <input
-              type="number"
-              matInput
-              placeholder="1"
-              [formControl]="order" />
-            <mat-hint>Empty will be added at end</mat-hint>
-          </mat-form-field>
-        </div>
+      <ng-container *ngSwitchCase="'edit'">
+        <ng-container *ngTemplateOutlet="formRef"></ng-container>
         <game-editor
           [editorExtension]="'md'"
           [content]="chapter?.content?.join('\\n')!"></game-editor>
-      </div>
-      <div *ngIf="createMode">
+      </ng-container>
+      <ng-container *ngSwitchCase="'create'">
+        <ng-container *ngTemplateOutlet="formRef"></ng-container>
+        <game-editor
+          [editorExtension]="'md'"
+          [content]="'please add content'"></game-editor>
+      </ng-container>
+      <ng-container
+        *ngTemplateOutlet="
+          editorControls;
+          context: { mode: mode }
+        "></ng-container>
+      <!-- reuse the chaptor editor controls -->
+      <ng-template #editorControls let-mode="mode">
+        <ng-container *ngIf="mode === 'default'">
+          <div class="chapter__controls">
+            <button class="chapter__controls__btn" gameToggleAnswer>
+              Show me
+            </button>
+            <div class="chapter__controls__control">
+              <div (click)="onSelectNextChapter()">Next</div>
+              <mat-icon class="chapter__controls__arrow-forward"
+                >arrow_forward</mat-icon
+              >
+            </div>
+          </div>
+          <div class="chapter__editor" (click)="updateMode('edit')">
+            <div class="chapter__editor__edit">
+              <mat-icon class="chapter__editor__icon">edit</mat-icon>Edit this
+              chapter
+            </div>
+            <div class="chapter__editor__edit" (click)="updateMode('create')">
+              <mat-icon class="chapter__editor__icon">library_add</mat-icon
+              >Create
+            </div>
+          </div>
+        </ng-container>
         <div
-          class="chapter__form"
-          style="display: flex; justify-content: space-between;">
+          class="chapter__editor"
+          [style.display]="mode === 'default' ? 'none' : ''">
+          <div
+            class="chapter__editor__edit"
+            (click)="
+              updateMode('default');
+              mode === 'edit' ? onSaveChapter() : onCreateChapter()
+            ">
+            <mat-icon class="chapter__editor__icon">save</mat-icon>Save
+          </div>
+          <div class="chapter__editor__edit" (click)="updateMode('default')">
+            <mat-icon class="chapter__editor__icon">backspace</mat-icon>Back
+          </div>
+          <div
+            *ngIf="mode === 'edit'"
+            class="chapter__editor__edit"
+            (click)="onDeleteChapter()">
+            <mat-icon class="chapter__editor__icon">delete</mat-icon>Delete
+          </div>
+        </div>
+      </ng-template>
+      <!-- reuse the form for the editor mode and the create mode -->
+      <ng-template #formRef>
+        <div class="chapter__form">
           <mat-form-field appearance="fill">
             <mat-label>Enter chapter title</mat-label>
             <input
@@ -89,79 +127,48 @@ import { FormControl } from '@angular/forms';
             <mat-hint>Empty will be added at end</mat-hint>
           </mat-form-field>
         </div>
-        <game-editor
-          [editorExtension]="'md'"
-          [content]="'Please add your content'"></game-editor>
-      </div>
-      <div class="chapter__controls" *ngIf="!editMode">
-        <button class="chapter__controls__btn" gameToggleAnswer>Show me</button>
-        <div class="chapter__controls__control">
-          <div (click)="onSelectNextChapter()">Next</div>
-          <mat-icon class="chapter__controls__arrow-forward"
-            >arrow_forward</mat-icon
-          >
-        </div>
-      </div>
-      <div
-        class="chapter__editor"
-        *ngIf="!editMode && !createMode"
-        (click)="onEditChapter()">
-        <div class="chapter__editor__edit">
-          <mat-icon class="chapter__editor__icon">edit</mat-icon>Edit this
-          chapter
-        </div>
-        <div class="chapter__editor__edit" (click)="onCreateChapterMode()">
-          <mat-icon class="chapter__editor__icon">library_add</mat-icon>Create
-        </div>
-      </div>
-      <div *ngIf="editMode" class="chapter__editor">
-        <div class="chapter__editor__edit" (click)="onSaveChapter()">
-          <mat-icon class="chapter__editor__icon">save</mat-icon>Save
-        </div>
-        <div class="chapter__editor__edit" (click)="onEditChapter()">
-          <mat-icon class="chapter__editor__icon">backspace</mat-icon>Back
-        </div>
-        <div class="chapter__editor__edit" (click)="onDeleteChapter()">
-          <mat-icon class="chapter__editor__icon">delete</mat-icon>Delete
-        </div>
-      </div>
-      <div *ngIf="createMode" class="chapter__editor">
-        <div class="chapter__editor__edit" (click)="onCreateChapter()">
-          <mat-icon class="chapter__editor__icon">save</mat-icon>Save
-        </div>
-        <div class="chapter__editor__edit" (click)="onCreateChapterMode()">
-          <mat-icon class="chapter__editor__icon">backspace</mat-icon>Back
-        </div>
-      </div>
+      </ng-template>
     </div>
   `,
   styleUrls: ['../../../styles.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class ChapterComponent {
+export class ChapterComponent implements OnDestroy {
   @Input() chapter: Chapter | null = null;
   @Output() nextChapter = new EventEmitter<number>();
   @Output() deleteChapter = new EventEmitter<number>();
-  editMode = false;
-  createMode = false;
   title = new FormControl('');
   order = new FormControl();
+  mode: 'edit' | 'create' | 'default' = 'default';
+  private destroy$ = new Subject<void>();
 
   constructor(
     private chapterEntityService: ChapterEntityService,
-    private editorService: EditorService
+    private editorService: EditorService,
+    private changeDetectorRef: ChangeDetectorRef
   ) {}
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  updateMode(currentMode: 'edit' | 'create' | 'default') {
+    console.warn('update the mode to: ', currentMode);
+    this.mode = currentMode;
+    if (currentMode === 'edit') {
+      this.title.setValue(this.chapter?.title || '');
+      this.order.setValue(this.chapter?.order || '');
+    } else if (currentMode === 'default') {
+      this.title.setValue('');
+      this.order.setValue('');
+    }
+    this.changeDetectorRef.detectChanges();
+  }
 
   onSelectNextChapter() {
     if (!this.chapter) return;
     this.nextChapter.emit(this.chapter.id + 1);
-  }
-
-  onEditChapter() {
-    if (!this.chapter) return;
-    this.editMode = !this.editMode;
-    this.title.setValue(this.chapter.title);
-    this.order.setValue(this.chapter.order);
   }
 
   onSaveChapter() {
@@ -178,28 +185,24 @@ export class ChapterComponent {
               order: this.order.value,
               content: content.split('\n'),
             };
-            // switch edit mode to false
-            this.editMode = false;
+            // update the mode
+            this.updateMode('default');
             // update the current chapter
             this.chapter = chapterToUpdate as Chapter;
             // update the chapter in the store
             this.chapterEntityService.update(chapterToUpdate);
-          })
+          }),
+          takeUntil(this.destroy$)
         );
       })
-      .subscribe()
-      .unsubscribe();
-  }
-
-  onCreateChapterMode() {
-    this.createMode = !this.createMode;
+      .subscribe();
   }
 
   onCreateChapter() {
     this.editorService.mdContentSyncObservable
       .pipe(
         withLatestFrom(this.chapterEntityService.entities$),
-        first(),
+        take(1),
         tap(([content, chapters]) => {
           const newChapter: Chapter = {
             id: chapters.length + 1,
@@ -209,15 +212,15 @@ export class ChapterComponent {
           };
           // add the new chapter to the store
           this.chapterEntityService.add(newChapter);
-          // switch edit mode to false
-          this.createMode = false;
+          // update the mode
+          this.updateMode('default');
           // reset the form
           this.title = new FormControl('');
           this.order = new FormControl();
-        })
+        }),
+        takeUntil(this.destroy$)
       )
-      .subscribe()
-      .unsubscribe();
+      .subscribe();
   }
 
   onDeleteChapter() {
@@ -225,6 +228,6 @@ export class ChapterComponent {
     // since it's necessary to change title as well
     // we pass the whole chapter to the upper-level component
     this.deleteChapter.emit(this.chapter.id);
-    this.editMode = false;
+    this.updateMode('default');
   }
 }
